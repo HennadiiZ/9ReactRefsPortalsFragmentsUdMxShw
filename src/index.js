3dcb86e5f854b63f266e8f35f07f903345b7ae6e
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2022-12-27 22:35:05 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+// 09 Diving Deeper_ Working with Fragments, Portals & _Refs_
+    // 105 Working with Portals
